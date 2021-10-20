577fbfe326fd6deb0929f6ce0880cfd27bd96c66
ConnectionError <dey78673@gmail.com>
2021-10-20 19:49:32 +0530

    all done new list genre and featured

--- src/components/Library/Songs/Songs.js
+++ src/components/Library/Songs/Songs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Songs.css";
 
 const Songs = ({ favPlayLists, newPlayLists, genrePayLists }) => {
   console.log({ favPlayLists, newPlayLists, genrePayLists });
@@ -7,17 +8,17 @@ const Songs = ({ favPlayLists, newPlayLists, genrePayLists }) => {
     return (
       <>
         <h1>Featured Playlists:</h1>
-        <div className="overflow-hidden">
-          <div className="flex">
+        <div className="overflow-auto">
+          <div className="songs gap-3">
             {favPlayLists.playlists &&
               favPlayLists.playlists.items.map((song) => (
-                <div key={song.id} className="">
+                <div key={song.id} className="cursor-pointer song">
                   <img
                     src={song.images[0].url}
                     width="50"
                     alt={song.description}
                   />
-                  <p>{song.description}</p>
+                  <p className="overflow-hidden">{song.description}</p>
                 </div>
               ))}
           </div>
@@ -28,12 +29,42 @@ const Songs = ({ favPlayLists, newPlayLists, genrePayLists }) => {
 
   // New Release code
   else if (newPlayLists) {
-    return <div>code 2</div>;
+    return (
+      <>
+        <h1>New Released Playlists:</h1>
+        <div className="overflow-auto">
+          <div className="songs gap-3">
+            {newPlayLists.albums &&
+              newPlayLists.albums.items.map((song) => (
+                <div key={song.id} className="cursor-pointer song">
+                  <img src={song.images[0].url} width="50" alt={song.id} />
+                  <p className="overflow-hidden">{song.artists[0].name}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+      </>
+    );
   }
 
   // New genre code
   else if (genrePayLists) {
-    return <div>code 3</div>;
+    return (
+      <>
+        <h1>Genre Playlists:</h1>
+        <div className="overflow-auto">
+          <div className="songs gap-3">
+            {genrePayLists.genres &&
+              genrePayLists.genres.map((song, index) => (
+                <div key={index} className="cursor-pointer song-genre">
+                  {/* <img src={song.images[0].url} width="50" alt={song.id} /> */}
+                  <p className="overflow-hidden">{song}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+      </>
+    );
   }
   return <div>Songs component</div>;
 };
